649912b77e928e83f1ec9f53eeda08ead4179d05
Ivan Gechev <44908454+Banovvv@users.noreply.github.com>
2022-06-23 10:21:05 +0000

    Update 04.InsertRecordsInBothTables.sql

--- MS SQL/Databases Introduction/Minions/Minions/04.InsertRecordsInBothTables.sql
+++ MS SQL/Databases Introduction/Minions/Minions/04.InsertRecordsInBothTables.sql
@@ -1,9 +1,9 @@
-INSERT INTO dbo.Towns
+INSERT INTO Towns
 	VALUES (1, 'Sofia'),
 		   (2, 'Plovdiv'),
 		   (3, 'Varna');
 
-INSERT INTO dbo.Minions
+INSERT INTO Minions
 	VALUES(1, 'Kevin', 22, 1),
 		  (2, 'Bob', 15, 3),
 		  (3, 'Steward', NULL, 2);
