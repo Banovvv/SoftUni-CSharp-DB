7afbdea744ad6470de5b62c75c8c81c4ac4c9d85
Banovvv <ivan_banov@abv.bg>
2022-07-12 17:11:11 +0300

    13.MoviesDatabase

---
+++ MS SQL/Databases Introduction/Minions/Minions/13.MoviesDatabase.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE Movies
+
+CREATE TABLE [dbo].[Directors]
+(
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	DirectorName NVARCHAR(50),
+	Notes NVARCHAR(150)
+)
+
+CREATE TABLE [dbo].[Genres]
+(
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	GenreName NVARCHAR(20),	
+	Notes NVARCHAR(150)
+)
+
+CREATE TABLE [dbo].[Categories]
+(
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	CategoryName NVARCHAR(20),
+	Notes NVARCHAR(150)
+)
+
+CREATE TABLE [dbo].[Movies]
+(
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	Title NVARCHAR(50),
+	DirectorId INT NOT NULL,
+	CopyrightYear INT,
+	[Length] INT NOT NULL,
+	GenreId INT NOT NULL,	
+	CategoryId INT NOT NULL,	
+	Rating INT CHECK (Rating > 0 AND Rating <=10),	
+	Notes NVARCHAR(150)
+)
