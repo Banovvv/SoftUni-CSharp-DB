7cec779f8e648a5573684edcdd58f09d0f35171a
Banovvv <ivan_banov@abv.bg>
2022-07-16 16:12:17 +0300

    Last SoftUni query

---
+++ MS SQL/Built-in Functions/Built-in Functions/11.FindAllEmployeesWithRank2 .sql
@@ -0,0 +1,7 @@
+SELECT * FROM
+(SELECT [EmployeeID], [FirstName], [LastName], [Salary],
+	DENSE_RANK() OVER(PARTITION BY [Salary] ORDER BY [EmployeeID] ASC) AS RANK
+	FROM [dbo].[Employees]
+	WHERE [Salary] > 10000 AND [Salary] < 50000) AS SubQuery
+WHERE RANK = 2
+ORDER BY [Salary] DESC
