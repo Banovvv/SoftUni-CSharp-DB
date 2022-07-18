4a3e71e099406774443c2ead7800e9711bd15b30
Banovvv <ivan_banov@abv.bg>
2022-07-18 16:07:22 +0300

    01.EmployeeAddress

---
+++ MS SQL/Subqueries and Joins/Subqueries and Joins/01.EmployeeAddress.sql
@@ -0,0 +1,9 @@
+SELECT TOP(5)
+	[dbo].[Employees].[EmployeeId],
+	[dbo].[Employees].[JobTitle],
+	[dbo].[Employees].[AddressID],
+	[dbo].[Addresses].[AddressText] 
+FROM [dbo].[Employees]
+INNER JOIN [dbo].[Addresses]
+	ON [dbo].[Addresses].[AddressID] = [dbo].[Employees].[AddressID]
+ORDER BY [dbo].[Addresses].[AddressID] ASC
