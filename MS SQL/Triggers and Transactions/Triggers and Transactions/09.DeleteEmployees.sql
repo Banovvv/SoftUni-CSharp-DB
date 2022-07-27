d36953f38dc2df207bde0f7f80350e4f1ac1acf9
Banovvv <ivan_banov@abv.bg>
2022-07-27 14:51:24 +0300

    09.DeleteEmployees

---
+++ MS SQL/Triggers and Transactions/Triggers and Transactions/09.DeleteEmployees.sql
@@ -0,0 +1,31 @@
+CREATE TABLE [dbo].[DeletedEmployees]
+(
+    EmployeeId INT PRIMARY KEY IDENTITY,
+    FirstName NVARCHAR(50) NOT NULL,
+    LastName NVARCHAR(50) NOT NULL,
+    MiddleName NVARCHAR(50),
+    JobTitle NVARCHAR(50) NOT NULL,
+    DepartmentId INT NOT NULL,
+    Salary MONEY NOT NULL
+)
+
+GO
+
+CREATE TRIGGER
+	tr_InsertEmployeesOnDelete
+    on [dbo].[Employees]
+    AFTER DELETE AS
+    BEGIN
+        INSERT INTO
+			[dbo].[DeletedEmployees]
+        SELECT
+			[Deleted].[FirstName],
+			[Deleted].[LastName],
+			[Deleted].[MiddleName],
+			[Deleted].[JobTitle],
+			[Deleted].[DepartmentID],
+			[Deleted].[Salary]
+        FROM
+			[Deleted]
+    END
+GO
