450236e01514efc35dc7bad056b87ec736deec9e
Banovvv <ivan_banov@abv.bg>
2022-07-29 16:06:05 +0300

    Student.cs

--- Entity Framework Core/Entity Relations/01.StudentSystem/Data/Models/Student.cs
+++ Entity Framework Core/Entity Relations/01.StudentSystem/Data/Models/Student.cs
@@ -1,10 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace P01_StudentSystem.Data.Models
 {
-    internal class Student
+    public class Student
     {
+        public Student()
+        {
+
+        }
+
+        public int StudentId { get; set; }
+        // (up to 100 characters, unicode)
+        public string Name { get; set; }
+        // (exactly 10 characters, not unicode, not required)
+        public string PhoneNumber { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        // (not required)
+        public DateTime Birthday { get; set; }
     }
 }
