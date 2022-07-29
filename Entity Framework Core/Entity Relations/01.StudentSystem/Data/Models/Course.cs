5d28fe84493114ba2b5d171b0931ad271083c245
Banovvv <ivan_banov@abv.bg>
2022-07-29 16:10:56 +0300

    Course.cs

--- Entity Framework Core/Entity Relations/01.StudentSystem/Data/Models/Course.cs
+++ Entity Framework Core/Entity Relations/01.StudentSystem/Data/Models/Course.cs
@@ -1,10 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace P01_StudentSystem.Data.Models
 {
-    internal class Course
+    public class Course
     {
+        public Course()
+        {
+
+        }
+
+        public int CourseId { get; set; }
+        // (up to 80 characters, unicode)
+        public string Name { get; set; }
+        // (unicode, not required)
+        public string? Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
     }
 }
