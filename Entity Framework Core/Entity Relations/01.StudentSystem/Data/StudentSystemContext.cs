ccd97afe0c555d5de606a64ee5318aad9dda34c5
Banovvv <ivan_banov@abv.bg>
2022-07-30 08:52:57 +0300

    StudentSystemContext

--- Entity Framework Core/Entity Relations/01.StudentSystem/Data/StudentSystemContext.cs
+++ Entity Framework Core/Entity Relations/01.StudentSystem/Data/StudentSystemContext.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
 
 namespace P01_StudentSystem.Data
 {
-    public class StudentSystemContext
+    public class StudentSystemContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Student> Students { get; set; }
     }
 }
