bec0e554f8a1edf67fb76cd20ce50a3139684cec
Banovvv <ivan_banov@abv.bg>
2022-07-31 17:32:05 +0300

    Town and Country

---
+++ Entity Framework Core/Entity Relations/02.FootballBetting/Data/Models/Town.cs
@@ -0,0 +1,9 @@
+﻿namespace P03_FootballBetting.Data.Models
+{
+    public class Town
+    {
+        public int TownId { get; set; }
+        public string Name { get; set; }
+        public int CountryId { get; set; }
+    }
+}
