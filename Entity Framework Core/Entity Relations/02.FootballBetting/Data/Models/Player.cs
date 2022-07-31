9966f5d5046ad2f734990252b46c5bc633de856e
Banovvv <ivan_banov@abv.bg>
2022-07-31 17:34:46 +0300

    Player and position

---
+++ Entity Framework Core/Entity Relations/02.FootballBetting/Data/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace P03_FootballBetting.Data.Models
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public int SquadNumber { get; set; }
+        public int TeamId { get; set; }
+        public int PositionId { get; set; }
+        public bool IsInjured { get; set; }
+    }
+}
---
+++ Entity Framework Core/Entity Relations/02.FootballBetting/Data/Models/Position.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Position
+    {
+        public int PositionId { get; set; }
+        public string Name { get; set; }
+    }
+}
