05e65932bec77c340e34240ff5bc9b7ad4b771c0
Banovvv <ivan_banov@abv.bg>
2022-08-01 08:22:04 +0300

    Work on FootballBettingContext

--- Entity Framework Core/Entity Relations/02.FootballBetting/Data/FootballBettingContext.cs
+++ Entity Framework Core/Entity Relations/02.FootballBetting/Data/FootballBettingContext.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using P03_FootballBetting.Data.Models;
 
 namespace P03_FootballBetting.Data
 {
-    internal class FootballBettingContext
+    public class FootballBettingContext : DbContext
     {
+        public DbSet<Bet> Bets { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<PlayerStatistic> PlayerStatistics { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
