c8a967287b691ad01e6889442cab36d9c289365a
Banovvv <ivan_banov@abv.bg>
2022-08-01 07:19:29 +0300

    team

--- Entity Framework Core/Entity Relations/02.FootballBetting/Data/Models/Team.cs
+++ Entity Framework Core/Entity Relations/02.FootballBetting/Data/Models/Team.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace P03_FootballBetting.Data.Models
+﻿namespace P03_FootballBetting.Data.Models
 {
     public class Team
     {
+        public int TeamId { get; set; }
+        public string Name { get; set; }
+        public string LogoUrl { get; set; }
+        public string Initials { get; set; }
+        public decimal Budget { get; set; }
+        public int PrimaryKitColorId { get; set; }
+        public int SecondaryKitColorId { get; set; }
+        public int TownId { get; set; }
     }
 }
