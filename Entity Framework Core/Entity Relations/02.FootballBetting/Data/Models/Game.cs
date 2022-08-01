12bdb4cdecca7d3839e0a96bd2ef2f1a7e0d5e77
Banovvv <ivan_banov@abv.bg>
2022-08-01 07:36:38 +0300

    Game.cs

---
+++ Entity Framework Core/Entity Relations/02.FootballBetting/Data/Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public int HomeTeamId { get; set; }
+        public int AwayTeamId { get; set; }
+        public int HomeTeamGoals { get; set; }
+        public int AwayTeamGoals { get; set; }
+        public DateTime DateTime { get; set; }
+        public double HomeTeamBetRate { get; set; }
+        public double AwayTeamBetRate { get; set; }
+        public double DrawBetRate { get; set; }
+        // Enum?
+        public int Result { get; set; }
+    }
+}
