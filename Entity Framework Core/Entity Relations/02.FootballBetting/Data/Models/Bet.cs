942b96f448a1b860a8edda5e7ecfb8942fe01da7
Banovvv <ivan_banov@abv.bg>
2022-08-01 07:38:53 +0300

    Bet

---
+++ Entity Framework Core/Entity Relations/02.FootballBetting/Data/Models/Bet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Bet
+    {
+        public int BetId { get; set; }
+        public decimal Amount { get; set; }
+        // Enum?
+        public int Prediction { get; set; }
+        public DateTime DateTime { get; set; }
+        public int UserId { get; set; }
+        public int GameId { get; set; }
+    }
+}
