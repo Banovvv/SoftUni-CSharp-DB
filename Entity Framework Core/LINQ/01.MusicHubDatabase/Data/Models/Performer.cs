8515a428a0615d682349da12b3a9d1e6bbd127b5
Banovvv <ivan_banov@abv.bg>
2022-08-09 19:33:27 +0300

    Producer and Performer

---
+++ Entity Framework Core/LINQ/01.MusicHubDatabase/Data/Models/Performer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MusicHub.Data.Models
+{
+    public class Performer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal NetWorth { get; set; }
+
+        public virtual ICollection<SongPerformer> PerformerSongs { get; set; }
+    }
+}
--- Entity Framework Core/LINQ/01.MusicHubDatabase/Data/Models/Producer.cs
+++ Entity Framework Core/LINQ/01.MusicHubDatabase/Data/Models/Producer.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace MusicHub.Data.Models
 {
     public class Producer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Pseudonym { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public virtual ICollection<Album> Albums { get; set; }
     }
 }
