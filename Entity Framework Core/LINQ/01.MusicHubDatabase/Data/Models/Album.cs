087a03a1d59fe572ab18032cc58ac43aa72761a3
Banovvv <ivan_banov@abv.bg>
2022-08-09 19:20:25 +0300

    Work on Album

--- Entity Framework Core/LINQ/01.MusicHubDatabase/Data/Models/Album.cs
+++ Entity Framework Core/LINQ/01.MusicHubDatabase/Data/Models/Album.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace MusicHub.Data.Models
 {
     public class Album
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price => Songs.Sum(x => x.Price);
+
+        public virtual ICollection<Song> Songs { get; set; }
     }
 }
