db9916e8615590d196042d9894a58396ab7a1c54
Banovvv <ivan_banov@abv.bg>
2022-08-09 19:06:25 +0300

    Initial commit

---
+++ Entity Framework Core/LINQ/01.MusicHubDatabase/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using MusicHub.Data;
+using MusicHub.Initializer;
+using System;
+
+namespace MusicHub
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context =
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            //Test your solutions here
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
