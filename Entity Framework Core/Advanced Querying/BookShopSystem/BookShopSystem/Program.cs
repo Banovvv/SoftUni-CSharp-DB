2ca110175d3fada6da3769fb8123426ec03057d6
Banovvv <ivan_banov@abv.bg>
2022-08-11 16:31:59 +0300

    2. Age Restriction

--- Entity Framework Core/Advanced Querying/BookShopSystem/BookShopSystem/Program.cs
+++ Entity Framework Core/Advanced Querying/BookShopSystem/BookShopSystem/Program.cs
@@ -1,17 +1,46 @@
 ﻿using BookShopSystem.Initializer;
 using BookShopSystem.Data;
 using System;
+using System.Text;
+using System.Linq;
+using BookShopSystem.Models.Enums;
 
 namespace BookShopSystem
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             using(var context = new BookShopContext())
             {
-                DbInitializer.ResetDatabase(context);
+                //DbInitializer.ResetDatabase(context);
+
+                // 2. Age Restriction
+                Console.WriteLine(GetBooksByAgeRestriction(context, "miNor"));
             }
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string ageGroup = null)
+        {
+            if (ageGroup == null)
+            {
+                ageGroup = Console.ReadLine();
+            }
+
+            var books = context.Books
+                    .ToList()
+                    .Where(x => x.AgeRestriction.ToString().Equals(ageGroup, StringComparison.OrdinalIgnoreCase))
+                    .Select(x => x.Title)
+                    .OrderBy(x => x);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine(book);
+            }
+
+            return sb.ToString().Trim();
         }
     }
 }
