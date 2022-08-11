476b3291f8505cbcb2e25f44bf4d465f7a82363c
Banovvv <ivan_banov@abv.bg>
2022-08-11 11:01:09 +0300

    Author class

--- Entity Framework Core/Advanced Querying/BookShopSystem/BookShopSystem.Data/BookShopContext.cs
+++ Entity Framework Core/Advanced Querying/BookShopSystem/BookShopSystem.Data/BookShopContext.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using BookShopSystem.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookShopSystem.Data
 {
@@ -15,12 +15,30 @@ namespace BookShopSystem.Data
 
         }
 
+        public virtual DbSet<Author> Authors { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseSqlServer(Configuration.GetConnectionString());
             }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>(author =>
+            {
+                author.Property(x => x.FirstName)
+                    .IsRequired(false)
+                    .HasMaxLength(50)
+                    .IsUnicode(true);
+
+                author.Property(x => x.LastName)
+                    .IsRequired(true)
+                    .HasMaxLength(50)
+                    .IsUnicode(true);
+            });
         }
     }
 }
