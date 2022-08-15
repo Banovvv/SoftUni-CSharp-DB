d429dace2947905a90f203b8f29ee4068d59bb3b
Banovvv <ivan_banov@abv.bg>
2022-08-15 15:20:34 +0300

    Product.cs

--- Entity Framework Core/JavaScript Object Notation - JSON/ProductsShop/ProductsShop.Data/ProductsShopContext.cs
+++ Entity Framework Core/JavaScript Object Notation - JSON/ProductsShop/ProductsShop.Data/ProductsShopContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProductsShop.Models;
 
 namespace ProductsShop.Data
 {
@@ -12,12 +13,39 @@ namespace ProductsShop.Data
         {
         }
 
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseSqlServer(Configuration.GetConnectionString());
             }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(user =>
+            {
+                user.Property(x=>x.FirstName)
+                    .IsRequired(false)
+                    .HasMaxLength(50)
+                    .IsUnicode(true);
+
+                user.Property(x => x.LastName)
+                    .IsRequired(true)
+                    .HasMaxLength(50)
+                    .IsUnicode(true);
+            });
+
+            modelBuilder.Entity<Product>(product =>
+            {
+                product.Property(x => x.Name)
+                    .IsRequired(true)
+                    .HasMaxLength(100)
+                    .IsUnicode(true);
+            });
         }
     }
 }
