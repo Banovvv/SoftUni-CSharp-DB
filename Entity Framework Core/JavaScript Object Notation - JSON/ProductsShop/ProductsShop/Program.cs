9c5f3d2a0b0c68478f990a0de0edfdcb272d1d22
Banovvv <ivan_banov@abv.bg>
2022-08-15 12:55:59 +0300

    initial commit

---
+++ Entity Framework Core/JavaScript Object Notation - JSON/ProductsShop/ProductsShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductsShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
