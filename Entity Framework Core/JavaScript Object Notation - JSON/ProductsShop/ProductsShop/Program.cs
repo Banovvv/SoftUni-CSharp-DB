07a85c1b53f770bbcff28b3ed3fc3781ab5812c4
Banovvv <ivan_banov@abv.bg>
2022-08-16 16:20:42 +0300

    Starting work on json files

--- Entity Framework Core/JavaScript Object Notation - JSON/ProductsShop/ProductsShop/Program.cs
+++ Entity Framework Core/JavaScript Object Notation - JSON/ProductsShop/ProductsShop/Program.cs
@@ -6,12 +6,20 @@ namespace ProductsShop
 {
     public class Program
     {
+        private const string DatasetsDirectoryPath = "../../../Datasets";
+        private const string ResultsDirectoryPath = "../../../Datasets/Results";
+
         static void Main(string[] args)
         {
             using (var context = new ProductsShopContext())
             {
                 DbInitializer.Initialize(context);
             }
+        }
+
+        public static string ImportUsers(ProductsShopContext context, string inputJson)
+        {
+
         }
     }
 }
