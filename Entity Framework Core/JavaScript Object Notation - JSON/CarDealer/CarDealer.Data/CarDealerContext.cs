3f0516fedbc003f27e12f5a2ed14c21595101ae4
Banovvv <ivan_banov@abv.bg>
2022-08-18 10:43:17 +0300

    dbcontext skeleton

--- Entity Framework Core/JavaScript Object Notation - JSON/CarDealer/CarDealer.Data/CarDealerContext.cs
+++ Entity Framework Core/JavaScript Object Notation - JSON/CarDealer/CarDealer.Data/CarDealerContext.cs
@@ -1,10 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
 
 namespace CarDealer.Data
 {
     public class CarDealerContext : DbContext
     {
-        
+        public CarDealerContext()
+        {
+        }
+
+        public CarDealerContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
     }
 }
