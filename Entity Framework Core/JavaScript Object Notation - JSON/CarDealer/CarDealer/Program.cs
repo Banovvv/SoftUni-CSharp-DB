b013565958ce1313512ac722e93bea2459e09a54
Banovvv <ivan_banov@abv.bg>
2022-08-18 13:17:01 +0300

    Import Suppliers

--- Entity Framework Core/JavaScript Object Notation - JSON/CarDealer/CarDealer/Program.cs
+++ Entity Framework Core/JavaScript Object Notation - JSON/CarDealer/CarDealer/Program.cs
@@ -1,17 +1,46 @@
 ﻿using CarDealer.Data;
 using CarDealer.Initializer;
+using CarDealer.Models;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CarDealer
 {
     internal class Program
     {
+        private const string DatasetsDirectoryPath = "../../../Datasets";
+        private const string ResultsDirectoryPath = "../../../Datasets/Exports";
+        private static JsonSerializerSettings serializerSettings = new JsonSerializerSettings()
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+            Formatting = Formatting.Indented
+        };
+
         static void Main(string[] args)
         {
             using(var context = new CarDealerContext())
             {
-                DbInitializer.Initialize(context);
+                //DbInitializer.Initialize(context);
+
+                // 01. Import Suppliers
+                var suppliersJson = File.ReadAllText($"{DatasetsDirectoryPath}/suppliers.json");
+                Console.WriteLine(ImportSuppliers(context, suppliersJson));
             }
         }
+
+        #region Import Methods
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson, serializerSettings);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+        }
+        #endregion
     }
 }
