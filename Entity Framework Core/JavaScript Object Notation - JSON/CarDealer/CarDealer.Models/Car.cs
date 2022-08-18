3c6c4dea3771869b54fe81568b85ed350bfccd2a
Banovvv <ivan_banov@abv.bg>
2022-08-18 13:10:10 +0300

    datasets files

--- Entity Framework Core/JavaScript Object Notation - JSON/CarDealer/CarDealer.Models/Car.cs
+++ Entity Framework Core/JavaScript Object Notation - JSON/CarDealer/CarDealer.Models/Car.cs
@@ -12,7 +12,7 @@ namespace CarDealer.Models
         public int Id { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
-        public int TravelledDistance { get; set; }
+        public long TravelledDistance { get; set; }
 
         public virtual ICollection<PartCar> CarParts { get; set; }
     }
