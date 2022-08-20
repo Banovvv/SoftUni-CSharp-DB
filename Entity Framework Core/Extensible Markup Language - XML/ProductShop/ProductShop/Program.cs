22c09ac5b50982646fddd352926b813d68cd6543
Banovvv <ivan_banov@abv.bg>
2022-08-20 08:05:34 +0300

    Setting up

--- Entity Framework Core/Extensible Markup Language - XML/ProductShop/ProductShop/Program.cs
+++ Entity Framework Core/Extensible Markup Language - XML/ProductShop/ProductShop/Program.cs
@@ -1,16 +1,21 @@
 ﻿using ProductShop.Data;
 using ProductShop.Initializer;
 using System;
+using System.Xml.Linq;
 
 namespace ProductShop
 {
     public class Program
     {
+        private const string DatasetsDirectoryPath = "../../../Datasets";
+        private const string ResultsDirectoryPath = "../../../Datasets/Exports";
         static void Main(string[] args)
         {
             using(var context = new ProductShopContext())
             {
-                DbInitializer.Initialize(context);
+                //DbInitializer.Initialize(context);
+
+                XDocument document = XDocument.Load($"{DatasetsDirectoryPath}/users.xml");
             }
         }
     }
