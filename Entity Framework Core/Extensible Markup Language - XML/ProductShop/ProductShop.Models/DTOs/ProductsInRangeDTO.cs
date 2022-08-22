8fd366af54e15183e3a42e76312d488133ab5c44
Banovvv <ivan_banov@abv.bg>
2022-08-22 13:57:05 +0300

    5. Products In Range

---
+++ Entity Framework Core/Extensible Markup Language - XML/ProductShop/ProductShop.Models/DTOs/ProductsInRangeDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.Models.DTOs
+{
+    [XmlType("Product")]
+    public class ProductsInRangeDTO
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+        [XmlElement("buyer")]
+        public string Buyer { get; set; }
+
+        public bool ShouldSerializeBuyer()
+        {
+            return !string.IsNullOrWhiteSpace(Buyer);
+        }
+    }
+}
--- Entity Framework Core/Extensible Markup Language - XML/ProductShop/ProductShop/Program.cs
+++ Entity Framework Core/Extensible Markup Language - XML/ProductShop/ProductShop/Program.cs
@@ -1,10 +1,12 @@
 ﻿using ProductShop.Data;
 using ProductShop.Initializer;
 using ProductShop.Models;
+using ProductShop.Models.DTOs;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Xml.Serialization;
 
 namespace ProductShop
@@ -17,9 +19,40 @@ namespace ProductShop
         {
             using (var context = new ProductShopContext())
             {
-                DbInitializer.Initialize(context);
-                DbInitializer.Seed(context);                
+                //DbInitializer.Initialize(context);
+                //DbInitializer.Seed(context);
+
+                // 5. Products In Range
+                File.WriteAllText($"{ResultsDirectoryPath}/products-in-range.xml", GetProductsInRange(context));
             }
+        }
+
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            IEnumerable<ProductsInRangeDTO> products = context.Products.Where(x => x.Price >= 500 && x.Price <= 1000)
+                .OrderBy(x => x.Price)
+                .Take(10)
+                .Select(x => new ProductsInRangeDTO
+                {
+                    Name = x.Name,
+                    Price = x.Price,
+                    Buyer = x.Buyer.FirstName + " " + x.Buyer.LastName
+                })
+                .ToList();
+
+            var serializer = new XmlSerializer(typeof(List<ProductsInRangeDTO>), new XmlRootAttribute("Products"));
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add("", "");
+
+            StringBuilder sb = new StringBuilder();
+
+            using (var writer = new StringWriter(sb))
+            {
+                serializer.Serialize(writer, products, namespaces);
+            }
+
+            return sb.ToString().Trim();
         }
     }
 }
