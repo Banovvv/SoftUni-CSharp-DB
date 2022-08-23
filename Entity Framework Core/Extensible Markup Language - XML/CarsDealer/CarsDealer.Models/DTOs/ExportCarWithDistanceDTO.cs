8c3f2f5f81d9b8b57f57e953110080ffd89d6c0a
Banovvv <ivan_banov@abv.bg>
2022-08-23 07:48:25 +0300

    6. Cars With Distance

---
+++ Entity Framework Core/Extensible Markup Language - XML/CarsDealer/CarsDealer.Models/DTOs/ExportCarWithDistanceDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace CarsDealer.Models.DTOs
+{
+    public class ExportCarWithDistanceDTO
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+        [XmlElement("model")]
+        public string Model { get; set; }
+        [XmlElement("travelled-distance")]
+        public long TravelledDistance { get; set; }
+    }
+}
--- Entity Framework Core/Extensible Markup Language - XML/CarsDealer/CarsDealer/Program.cs
+++ Entity Framework Core/Extensible Markup Language - XML/CarsDealer/CarsDealer/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Xml.Serialization;
 
 namespace CarsDealer
@@ -18,9 +19,40 @@ namespace CarsDealer
         {
             using (var context = new CarDealerContext())
             {
-                DbInitializer.Initialize(context);
-                DbInitializer.Seed(context);
+                //DbInitializer.Initialize(context);
+                //DbInitializer.Seed(context);
+
+                // 6. Cars With Distance
+                File.WriteAllText($"{ResultsDirectoryPath}/cars.xml", GetCarsWithDistance(context));
             }
+        }
+
+        public static string GetCarsWithDistance(CarDealerContext context)
+        {
+            IEnumerable<ExportCarWithDistanceDTO> cars = context.Cars.Where(x => x.TravelledDistance >= 200000)
+                .OrderBy(x => x.Make)
+                .OrderBy(x => x.Model)
+                .Take(10)
+                .Select(x=> new ExportCarWithDistanceDTO
+                {
+                    Make = x.Make,
+                    Model = x.Model,
+                    TravelledDistance = x.TravelledDistance
+                })
+                .ToList();
+
+            var serializer = new XmlSerializer(typeof(List<ExportCarWithDistanceDTO>), new XmlRootAttribute("cars"));
+
+            StringBuilder sb = new StringBuilder();
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add("", "");
+
+            using (StringWriter writer = new StringWriter(sb))
+            {
+                serializer.Serialize(writer, cars, namespaces);
+            }
+
+            return sb.ToString().Trim();
         }
     }
 }
