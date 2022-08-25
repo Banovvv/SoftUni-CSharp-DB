175f282aba76118a35870013f004f0d4e8f50621
Banovvv <ivan_banov@abv.bg>
2022-08-25 22:05:03 +0300

    Service interfaces

---
+++ Entity Framework Core/Code-First/HospitalDatabase/HospitalDatabase.Services/IPatientService.cs
@@ -0,0 +1,10 @@
+﻿namespace HospitalDatabase.Services
+{
+    public interface IPatientService
+    {
+        void Add(string firstName, string lastName, string address, string email, bool hasInsurance);
+        void Remove(string firstName, string lastName);
+        void PrescribeMedication(string firstName, string lastName, string medicationName);
+        void RemoveMedication(string firstName, string lastName, string medicationName);
+    }
+}
