0c0be29944093a4ec9ee7d23da39fcc9bb2e696b
Banovvv <ivan_banov@abv.bg>
2022-08-26 08:24:24 +0300

    Service interfaces

---
+++ Entity Framework Core/Code-First/HospitalDatabase/HospitalDatabase.Services/IVisitationService.cs
@@ -0,0 +1,7 @@
+﻿namespace HospitalDatabase.Services
+{
+    public interface IVisitationService
+    {
+        void Add(string date, int doctorId, int patientId, string comments = null);
+    }
+}
