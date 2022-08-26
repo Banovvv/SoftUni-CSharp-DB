f20fbc80e00ed25a7831239a1d4c576d0391b0f2
Banovvv <ivan_banov@abv.bg>
2022-08-26 08:47:39 +0300

    Empty methods

--- Entity Framework Core/Code-First/HospitalDatabase/HospitalDatabase/Program.cs
+++ Entity Framework Core/Code-First/HospitalDatabase/HospitalDatabase/Program.cs
@@ -31,16 +31,59 @@ namespace HospitalDatabase
                 {
                     switch (choice)
                     {
-                        case 1: break;
-                        case 2: break;
-                        case 3: break;
-                        case 4: break;
-                        case 5: break;
-                        case 6: break;
-                        case 7: break;
+                        case 1:
+                            AddPatient();
+                            break;
+                        case 2:
+                            RemovePatient();
+                            break;
+                        case 3:
+                            PrescribeMedication();
+                            break;
+                        case 4:
+                            RemoveMedication(); 
+                            break;
+                        case 5:
+                            AddDiagnose();
+                            break;
+                        case 6:
+                            RemoveDiagnose();
+                            break;
+                        case 7:
+                            AddVisitation();
+                            break;
                     }
                 }
             }
+        }
+
+        private static void AddPatient()
+        {
+            throw new NotImplementedException();
+        }
+        private static void RemovePatient()
+        {
+            throw new NotImplementedException();
+        }
+        private static void PrescribeMedication()
+        {
+            throw new NotImplementedException();
+        }
+        private static void RemoveMedication()
+        {
+            throw new NotImplementedException();
+        }
+        private static void AddDiagnose()
+        {
+            throw new NotImplementedException();
+        }
+        private static void RemoveDiagnose()
+        {
+            throw new NotImplementedException();
+        }
+        private static void AddVisitation()
+        {
+            throw new NotImplementedException();
         }
     }
 }
