b6bd9627e4053d7a7e4a6770aca8259ce6ae8948
Banovvv <ivan_banov@abv.bg>
2022-08-27 12:42:59 +0300

    Initial commit

---
+++ Entity Framework Core/Code-First/SalesDatabase/SalesDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesDatabase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
