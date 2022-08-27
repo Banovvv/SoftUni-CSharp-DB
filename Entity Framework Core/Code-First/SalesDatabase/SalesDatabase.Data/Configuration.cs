730063efda9e369b6aba0d8040e56f51a3677a9b
Banovvv <ivan_banov@abv.bg>
2022-08-27 12:55:43 +0300

    DbContext skeleton

---
+++ Entity Framework Core/Code-First/SalesDatabase/SalesDatabase.Data/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesDatabase.Data
+{
+    internal static class Configuration
+    {
+        internal static string GetConnectionString()
+        {
+            return @"Server=.\SQLEXPRESS;Database=SalesDatabase;Integrated Security=true;";
+        }
+    }
+}
--- Entity Framework Core/Code-First/SalesDatabase/SalesDatabase.Data/SalesDataContext.cs
+++ Entity Framework Core/Code-First/SalesDatabase/SalesDatabase.Data/SalesDataContext.cs
@@ -1,8 +1,27 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace SalesDatabase.Data
 {
-    public class SalesDataContext
+    public class SalesDataContext : DbContext
     {
+        public SalesDataContext()
+        {
+        }
+
+        public SalesDataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
     }
 }
